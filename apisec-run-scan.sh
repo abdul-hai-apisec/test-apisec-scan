a8620109d34d9347c0f832543c5f23a3bf6fa8c4
abdul-hai-apisec <83706991+abdul-hai-apisec@users.noreply.github.com>
2021-10-22 16:48:15 +0530

    Update apisec-run-scan.sh

--- apisec-run-scan.sh
+++ apisec-run-scan.sh
@@ -6,34 +6,41 @@ PWD=$2
 PROJECT=$3
 JOB=$4
 REGION=$5
+OUTPUT_FILENAME=$6
 PARAM_SCRIPT=""
 if [ "$JOB" != "" ];
 then
-PARAM_SCRIPT="?jobName="+${JOB}
+PARAM_SCRIPT="?jobName="${JOB}
   if [ "$REGION" != "" ];
   then
-  PARAM_SCRIPT=PARAM_SCRIPT+"&region="+${REGION}
+  PARAM_SCRIPT=${PARAM_SCRIPT}"&region="${REGION}
   fi
 elif [ "$REGION" != "" ];
   then
-  PARAM_SCRIPT="?region="+${REGION}
+  PARAM_SCRIPT="?region="${REGION}
 fi
 
 
-token=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${USER}'", "password": "'${PWD}'"}' https://cloud.fxlabs.io/login | jq -r .token)
+token=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${USER}'", "password": "'${PWD}'"}' https://developer.apisec.ai/login | jq -r .token)
 
 echo "generated token is:" $token
 
-runId=$(curl --location --request POST "https://cloud.fxlabs.io/api/v1/runs/projectName/${PROJECT}${PARAM_SCRIPT}" --header "Authorization: Bearer "$token"" | jq -r '.["data"]|.id')
+echo "The request is https://developer.apisec.ai/api/v1/runs/projectName/${PROJECT}${PARAM_SCRIPT}"
+
+data=$(curl --location --request POST "https://developer.apisec.ai/api/v1/runs/projectName/${PROJECT}${PARAM_SCRIPT}" --header "Authorization: Bearer "$token"" | jq '.data')
+
+runId=$("$data" | jq '.id')
+projectId=$("$data" | jq '.job.project.id')
 
 echo "runId =" $runId
 if [ -z "$runId" ]
 then
           echo "RunId = " "$runId"
           echo "Invalid runid"
-          echo $(curl --location --request POST "https://cloud.fxlabs.io/api/v1/runs/projectName/${PROJECT}${PARAM_SCRIPT}" --header "Authorization: Bearer "$token"" | jq -r '.["data"]|.id')
+          echo $(curl --location --request POST "https://developer.apisec.ai/api/v1/runs/projectName/${PROJECT}${PARAM_SCRIPT}" --header "Authorization: Bearer "$token"" | jq -r '.["data"]|.id')
           exit 1
 fi
+
 
 
 taskStatus="WAITING"
@@ -46,7 +53,7 @@ while [ "$taskStatus" == "WAITING" -o "$taskStatus" == "PROCESSING" ]
                 sleep 5
                  echo "Checking Status...."
 
-                passPercent=$(curl --location --request GET "https://cloud.fxlabs.io/api/v1/runs/${runId}" --header "Authorization: Bearer "$token""| jq -r '.["data"]|.ciCdStatus')
+                passPercent=$(curl --location --request GET "https://developer.apisec.ai/api/v1/runs/${runId}" --header "Authorization: Bearer "$token""| jq -r '.["data"]|.ciCdStatus')
 
                         IFS=':' read -r -a array <<< "$passPercent"
 
@@ -58,10 +65,16 @@ while [ "$taskStatus" == "WAITING" -o "$taskStatus" == "PROCESSING" ]
 
                 if [ "$taskStatus" == "COMPLETED" ];then
             echo "------------------------------------------------"
-                       # echo  "Run detail link https://cloud.fxlabs.io/${array[7]}"
-                        echo  "Run detail link https://cloud.fxlabs.io${array[7]}"
+                       # echo  "Run detail link https://developer.apisec.ai/${array[7]}"
+                        echo  "Run detail link https://developer.apisec.ai${array[7]}"
                         echo "-----------------------------------------------"
                         echo "Job run successfully completed"
+                        if [ "$OUTPUT_FILENAME" != "" ];
+                         then
+                         sarifoutput=$(curl --location --request GET "https://developer.apisec.ai/api/v1/projects/${projectId}/sarif" --header "Authorization: Bearer "$token""| jq '.data')
+						 printf $sarifoutput >> $OUTPUT_FILENAME
+						 echo "SARIF output file created successfully"
+                        fi
                         exit 0
 
                 fi
@@ -72,7 +85,7 @@ echo "Task Status = " $taskStatus
  exit 1
 fi
 
-echo "$(curl --location --request GET "https://cloud.fxlabs.io/api/v1/runs/${runId}" --header "Authorization: Bearer "$token"")"
+echo "$(curl --location --request GET "https://developer.apisec.ai/api/v1/runs/${runId}" --header "Authorization: Bearer "$token"")"
 exit 1
 
 return 0
